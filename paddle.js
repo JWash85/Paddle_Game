4afe860d1a8cbf0ffd38c77aa755986e2fc976ae
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-03-07 22:23:49 -0500

    got movement to images

--- paddle.js
+++ paddle.js
@@ -2,22 +2,75 @@ const canvas = document.getElementById("gameBoard");
 const context = canvas.getContext("2d");
 //const leftPaddle = document.getElementById("left-paddle")
 //setting variables for my images
-let ballB = new Image()
-let leftP = new Image()
-let rightP = new Image()
 
-ballB.src = 'assets/Red Chip.png'
-leftP.src = 'assets/Yellow Chip.png'
-rightP.src = 'assets/Yellow Chip.png'
-
-//img.src = 'assets/Yellow Chip.png'
 /*Context is apart of Canvas API 
     Canvas API used for drawing graphics via JavaScript & HTML
     Can be used for animaiton, game graphics, data visualization,
     photo manipulation, and real-time video processing*/
-
 let scoreOne = 0
 let scoreTwo = 0
+
+let ball = new Image()
+let playerOne = new Image()
+let computer = new Image()
+
+ball.src = 'assets/Red Chip.png'
+playerOne.src = 'assets/Yellow Chip.png'
+computer.src = 'assets/Yellow Chip.png'
+
+//setting Ball Variables
+var bx = canvas.width/2;
+var by = canvas.height/2;
+var bWidth = 50;
+var bHeight = 50;
+var bSpeed = 2;
+var bGravity = 2;
+
+//Player 1 Paddle Variables
+var px = 10;
+var py = canvas.height/2 - 100/2;
+var pWidth = 10;
+var pHeight = 100;
+
+
+//Computer Paddle Variables
+var cx = canvas.width - 20;
+var cy = canvas.height/2 - 100/2;
+var cWidth = 10;
+var cHeight = 100;
+
+//use image for ball
+function draw(i, x, y, w, h) {
+    context.drawImage(i, x, y,  w, h)
+}
+
+
+//draw the net
+const net = {
+    x: canvas.width/2 - 2/2,
+    y: 0,
+    width: 4,
+    height: 10,
+    color: "#fff"
+}
+
+
+
+function drawRect(x,y,w,h, color){
+    context.fillStyle = color;
+    context.fillRect(x,y,w,h,color);
+}
+function drawNet(){
+    for (let i = 0; i <= canvas.height; i+=20){
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+}
+        
+    
+
+
+
+
 
 //Key movement
 window.addEventListener("keypress", doKeyDown, false);
@@ -33,7 +86,7 @@ function doKeyDown(e) {
     else if (key == "k" && playerTwo.y + playerTwo.height + playerTwo.gravity < canvas.height)
         playerTwo.y += playerTwo.gravity * 15;
 }
-class Element{
+/*class Element{
     constructor(options){
         this.i = options.i;
         this.x = options.x;
@@ -77,13 +130,10 @@ const ball = new Element({
     speed: 2,
     gravity: 2
 
-});
+});*/
 
 
-//use image for ball
-function draw(i, x, y, w, h) {
-    context.drawImage(i, x, y,  w, h)
-}
+
 //use image as paddle
 
 
@@ -93,16 +143,16 @@ function drawElement(element) {
 }
 
 
-//Player one score text
+//Player scores text
 function displayScoreOne() {
-    context.font = "18px Arial";
+    context.font = "50px Arial";
     context.fillStyle = "#fff";
-    context.fillText(scoreOne, canvas.width / 2 - 60, 30);
+    context.fillText(scoreOne, canvas.width / 2 - 65, 50);
 }
 function displayScoreTwo() {
-    context.font = "18px Arial";
+    context.font = "50px Arial";
     context.fillStyle = "#fff";
-    context.fillText(scoreTwo, canvas.width / 2 + 60, 30);
+    context.fillText(scoreTwo, canvas.width / 2 + 40, 50);
 }
 //ball bounce
 /*function ballBounce(){
@@ -116,6 +166,7 @@ function displayScoreTwo() {
     }
     ballWallCollision();
 }*/
+
 function ballBounce(){
     if(by + bGravity <= 0 || by + bGravity >= canvas.height) {
         bGravity = bGravity * -1;
@@ -127,14 +178,9 @@ function ballBounce(){
     }
     ballWallCollision();
 }
-var bx = 400;
-var by = 400/2;
-var bWidth = 50;
-var bHeight = 50;
-var bSpeed = 2;
-var bGravity = 2;
 
-function ballWallCollision(){
+
+/*function ballWallCollision(){
     if(
         (ball.y + ball.gravity <= playerTwo.y + playerTwo.height &&
         ball.x + ball.width + ball.speed >= playerTwo.x &&
@@ -155,19 +201,43 @@ function ballWallCollision(){
         ball.y += ball.gravity;
     }
     drawElements();
+}*/
+function ballWallCollision(){
+    if(
+        (by + bGravity <= cy + cHeight &&
+        bx + bWidth + bSpeed >= cx &&
+        by + bGravity > cy) ||
+        (by + bGravity > py &&
+        bx + bSpeed <= px + pWidth)
+    ){
+        bSpeed = bSpeed * -1;
+    } else if (bx+bSpeed < px ){
+        scoreTwo +=1;
+        bSpeed = bSpeed * -1;
+        bx = 100 + bSpeed;
+        by += bGravity;
+    }else if (bx+bSpeed > cx + cWidth){
+        scoreOne +=1;
+        bSpeed = bSpeed * -1;
+        bx = 100 + bSpeed;
+        by += bGravity;
+    }
+    drawElements();
 }
 
 function drawElements(){
     context.clearRect(0,0,canvas.width, canvas.height);
-    drawElement(playerOne);
-    drawElement(playerTwo);
-    drawElement(ball);
+    //drawElement(playerOne);
+    //drawElement(playerTwo);
+    //drawElement(ball);
     displayScoreOne();
     displayScoreTwo();
     setWinner();
-    draw(ballB,bx, by, bWidth, bHeight)
-    //draw(leftP, 10, 200, 50, 50 )
-    //draw(rightP, 675, 200, 50, 50 )
+    draw(ball,bx, by, bWidth, bHeight);
+    draw(playerOne, px, py, pWidth, pHeight);
+    draw(computer, cx, cy, cWidth, cHeight );
+    drawNet();
+    drawRect();
 }
 function loop() {
     ballBounce()
@@ -182,9 +252,3 @@ function setWinner(){
     }else if(scoreTwo == '5')
         winner.innerText = "Player Two Wins!"
 }
-var bx = 400;
-var by = 400/2;
-var bWidth = 50;
-var bHeight = 50;
-var bSpeed = 2;
-var bGravity = 2;
